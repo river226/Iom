27e4d85fcc25c4c19e496213e809269530bfc757
Jedidiah River Clemons-Johnson <river226@gmail.com>
2016-05-22 21:09:55 -0500

    Get IO working properly

--- src/Iom.hs
+++ src/Iom.hs
@@ -1,11 +1,19 @@
-import qualified Data.ByteString.Lazy.Char8 as C
+import qualified Data.ByteString.Lazy as BS
+import Data.Word
+import Data.Bits
 
 -- Copyright (c) 2016 Jedidiah River Clemons-Johnson @ www.jedidiahriver.com
 -- MIT License @ opensource.org/licenses/MIT
 
 main = do
-  x <- C.readFile "test.mp3" -- Generates bytestream
-  C.writeFile "fliptest.mp3" $ processMp3 x
-  print "complete"
+  x <- BS.readFile "test.mp3" -- Generates bytestream
+  BS.writeFile "fliptest.mp3" $ processMp3 x -- Breaks apart and writes adjusted file
+  print "complete" -- Lets you know the program is complete
 
-processMp3 file = file
+-- Start processing the file
+processMp3 :: BS.ByteString -> BS.ByteString
+processMp3 file = BS.pack $ processFrame $ BS.unpack file
+
+-- Start processing the Frame, breaking into header/CRC/Side information, and Data
+processFrame :: [word8] -> [word8]
+processFrame file = file
