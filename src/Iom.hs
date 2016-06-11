93d515f89c860935b38142c14a9722e00dbfbe70
Jedidiah River Clemons-Johnson <river226@gmail.com>
2016-06-10 20:40:44 -0500

    Work on CRC

--- src/Iom.hs
+++ src/Iom.hs
@@ -1,9 +1,19 @@
+-- qualified imports
 import qualified Data.ByteString.Lazy as BS
+
+-- normal imports
+import Data.ByteString.Builder.Prim
 import Data.Word
 import Data.Bits
 
 -- Copyright (c) 2016 Jedidiah River Clemons-Johnson @ www.jedidiahriver.com
 -- MIT License @ opensource.org/licenses/MIT
+
+one :: Word8
+one = fromInteger 1
+
+zero :: Word8
+zero = fromInteger 0
 
 main = do
   x <- BS.readFile "test.mp3" -- Generates bytestream
@@ -15,5 +25,19 @@ processMp3 :: BS.ByteString -> BS.ByteString
 processMp3 file = BS.pack $ processFrame $ BS.unpack file
 
 -- Start processing the Frame, breaking into header/CRC/Side information, and Data
-processFrame :: [word8] -> [word8]
-processFrame file = file
+processFrame :: [Word8] -> [Word8]
+processFrame [] = []
+processFrame (h1:h2:h3:h4:f) = h1 : h2 : h3 : h4 : (processCRC (getPrivateBit h2) f)
+
+getPrivateBit :: Word8 -> Word8
+getPrivateBit pb = (.&.) pb one
+
+--processCRC
+processCRC :: Word8 -> [Word8] -> [Word8]
+processCRC one (p1: p2: f) = p1 : p2 : processSideInformation f
+processCRC zero f = processSideInformation f
+
+--processSideInformation
+processSideInformation file = file
+
+--flipData
