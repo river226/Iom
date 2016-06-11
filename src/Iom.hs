d55d633700a8bae5578d24f412a1f95b374b107f
Jedidiah River Clemons-Johnson <river226@gmail.com>
2016-06-10 21:32:44 -0500

    Add Constants

--- src/Iom.hs
+++ src/Iom.hs
@@ -9,11 +9,37 @@ import Data.Bits
 -- Copyright (c) 2016 Jedidiah River Clemons-Johnson @ www.jedidiahriver.com
 -- MIT License @ opensource.org/licenses/MIT
 
+-- Constants
+seven :: Word8
+seven = fromInteger 7
+
+six :: Word8
+six = fromInteger 6
+
+five :: Word8
+five = fromInteger 5
+
+four :: Word8
+four = fromInteger 4
+
+three :: Word8
+three = fromInteger 3
+
+two :: Word8
+two = fromInteger 2
+
 one :: Word8
 one = fromInteger 1
 
 zero :: Word8
 zero = fromInteger 0
+
+-- AND bits to get desired values
+-- desiredBits should always be one of the constants zero -> seven
+getSetBits :: Word8 -> Word8 -> Word8
+getSetBits inputBits desiredBits = (.&.) inputBits desiredBits
+
+-- Start
 
 main = do
   x <- BS.readFile "test.mp3" -- Generates bytestream
@@ -27,10 +53,7 @@ processMp3 file = BS.pack $ processFrame $ BS.unpack file
 -- Start processing the Frame, breaking into header/CRC/Side information, and Data
 processFrame :: [Word8] -> [Word8]
 processFrame [] = []
-processFrame (h1:h2:h3:h4:f) = h1 : h2 : h3 : h4 : (processCRC (getPrivateBit h2) f)
-
-getPrivateBit :: Word8 -> Word8
-getPrivateBit pb = (.&.) pb one
+processFrame (h1:h2:h3:h4:f) = h1 : h2 : h3 : h4 : (processCRC (getSetBits h2 one) f)
 
 --processCRC
 processCRC :: Word8 -> [Word8] -> [Word8]
